2d79f591f0f5334c68906c1a87d054dd3a649c42
Raja <54889522+rajapandey@users.noreply.github.com>
2022-01-28 13:17:02 +0530

    Add files via upload

---
+++ problem4.js
@@ -0,0 +1,18 @@
+var a = parseInt(prompt("Enter the number"));
+
+if(a > 0)
+{
+  console.log("Number is positive")
+}
+
+if(a < 0)
+{
+  console.log("Number is negative")
+}
+
+if(a == 0)
+{
+  console.log("Number is zero")
+}
+
+
