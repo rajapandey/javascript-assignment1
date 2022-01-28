293fd9fa9726bdc6386739b35c1c5721cd6dec09
Raja <54889522+rajapandey@users.noreply.github.com>
2022-01-28 13:11:13 +0530

    Add files via upload

---
+++ problem3.js
@@ -0,0 +1,12 @@
+var a = 5
+var b = 8
+
+console.log("The initial vaule of a is " + a + " and the initial value of b is " + b)
+var temp = a;
+a = b;
+b = temp
+
+
+console.log("After swapping:")
+
+console.log("The vaule of a is " + a + " and the value of b is " + b)
